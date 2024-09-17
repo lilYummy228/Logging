e89c64b2b8202b9509d1fc9907955820618d34e3
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-17 12:37:51 +0500

    Добавьте файлы проекта.

---
+++ Logging/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pathfinder fileLogWritter = new Pathfinder
+                (new FileLogWritter());
+
+            Pathfinder consoleLogWritter = new Pathfinder
+                (new ConsoleLogWritter());
+
+            Pathfinder fridayFileLogWritter = new Pathfinder
+                (new DateLogWritter
+                (new FileLogWritter(), DayOfWeek.Friday));
+
+            Pathfinder fridayConsoleLogWritter = new Pathfinder
+                (new DateLogWritter
+                (new ConsoleLogWritter(), DayOfWeek.Friday));
+
+            Pathfinder totalLogWritter = new Pathfinder
+                (new TotalLogWritter
+                (new ConsoleLogWritter(), new DateLogWritter
+                (new FileLogWritter(), DayOfWeek.Friday)));
+        }
+    }
+
+    internal interface ILogger
+    {
+        void WriteError(string message);
+    }
+
+    class Pathfinder
+    {
+        private const string Message = "error";
+        private readonly ILogger _logger;
+
+        public Pathfinder(ILogger logger) =>
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        public void Find() =>
+            _logger.WriteError(Message);
+    }
+
+    class ConsoleLogWritter : ILogger
+    {
+        public void WriteError(string message) =>
+            Console.WriteLine(message);
+    }
+
+    class FileLogWritter : ILogger
+    {
+        private const string LogText = "log.txt";
+
+        public void WriteError(string message) =>
+            File.WriteAllText(LogText, message);
+    }
+
+    class DateLogWritter : ILogger
+    {
+        private readonly ILogger _logger;
+        private readonly DayOfWeek _dayOfWeek;
+
+        public DateLogWritter(ILogger logger, DayOfWeek dayOfWeek)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _dayOfWeek = dayOfWeek;
+        }
+
+        public void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == _dayOfWeek)
+                _logger.WriteError(message);
+        }
+    }
+
+    class TotalLogWritter : ILogger
+    {
+        private readonly ILogger _consoleLogger;
+        private readonly ILogger _fridayFileLogger;
+
+        public TotalLogWritter(ILogger consoleLogger, ILogger fridayFileLogger)
+        {
+            _consoleLogger = consoleLogger ?? throw new ArgumentNullException(nameof(consoleLogger));
+            _fridayFileLogger = fridayFileLogger ?? throw new ArgumentNullException(nameof(fridayFileLogger));
+        }
+
+        public void WriteError(string message)
+        {
+            _consoleLogger.WriteError(message);
+            _fridayFileLogger.WriteError(message);
+        }
+    }
+}
